1bb5cc6e781650dffb05ca84c095affb85c0ffe3
HamishPayne <68985098+HamishPayne@users.noreply.github.com>
2020-10-08 13:40:29 +1100

    Update FINAL_v4.js

--- Classroom/Week-10/FINAL_v4/FINAL_v4.js
+++ Classroom/Week-10/FINAL_v4/FINAL_v4.js
@@ -4,10 +4,7 @@ let pos = 0;
 let mic;
 
 function preload(){
-    //utopia = loadStrings('data/utopia_text.txt');
     utopiaCAPS = loadStrings('data/utopia_text_CAPS.txt');
-    //Monument = loadFont('data/MonumentGroteskTrial-Mono.otf');
-    //Serifbabe = loadFont('data/Serifbabe.otf');
     MGregular = loadFont('data/MGRegular.otf');
     
     Clouds = loadImage('data/blurred_clouds2.jpg');
@@ -16,9 +13,7 @@ function preload(){
     rock2 = loadImage('data/rock2.png');
     rock3 = loadImage('data/rock3.png');
     earth = loadImage('data/Earth_green.png');
-    
-    //EUGENIC = loadImage('data/EUGENIC_con1.png');
-    //CONSCIOUSNESS = loadImage('data/CONSCIOUSNESS_mar1.png');
+
     fulltext = loadImage('data/Phase_5.png');
 }
 
@@ -35,7 +30,6 @@ let value=255;
 
 function draw(){
   background(238,240,255);
-  //image(Clouds,0,0,5120,2880);
   textAlign(LEFT);
   imageMode(CENTER);
   
@@ -48,17 +42,8 @@ function draw(){
   
   noTint();
   image(rock,windowWidth/6,pos*3+windowWidth/6,1557/4,1139/4);
-  //image(rock2,windowWidth/3,pos*3,1557/4-windowWidth/2,1139/4);
-  //image(rock3,-500,pos*3+windowWidth,1557/4,1139/4);
   image(cloud,windowWidth/4,pos/2-250,3480/3,1336/3);
   image(cloud,-windowWidth/4,pos/3+300,3480/3,1336/3);
-  
-  //translate (0,-windowHeight/2+30);
-  //fill(0);
-  //textSize(30);
-  //textFont (MGregular);
-  //text ('PHYSICAL',-windowWidth/2+10,-windowHeight/2+30);
-  //text ('DIGITAL',windowWidth/2-120,-windowHeight/2+30);
 
 }
 
